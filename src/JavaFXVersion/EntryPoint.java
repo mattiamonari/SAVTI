a62a5c47ddfc701b9c8f62f7b46de2aa5e2b4dfe
Mattia Monari <mattiamonari1972@gmail.com>
2022-05-05 15:27:49 +0200

    Reorganized file

--- src/JavaFXVersion/EntryPoint.java
+++ src/JavaFXVersion/EntryPoint.java
@@ -2,8 +2,10 @@ package JavaFXVersion;
 
 import javafx.application.Application;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
+import java.awt.*;
 //TODO!!!!!!! Sistemare un po' il codice in generale, commentare in maniera più chiara e migliorare la GUI
 
 public class EntryPoint extends Application {
@@ -16,9 +18,10 @@ public class EntryPoint extends Application {
 
     @Override
     public void start(Stage stage) throws Exception {
+        Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
         stage.setTitle("sortingAlgorithmsVisualization");
         MainWindow mw = new MainWindow(stage);
-        Scene scene = new Scene(mw, 2000,1000);
+        Scene scene = new Scene(mw, screenSize.getWidth(), screenSize.getHeight());
         stage.setMaximized(true);
         stage.setScene(scene);
         stage.show();
