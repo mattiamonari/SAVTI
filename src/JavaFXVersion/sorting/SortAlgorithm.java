f8a9ead3874201519058a6023081c6b3eaf9b971
Mattia Monari <mattiamonari1972@gmail.com>
2022-05-05 15:28:18 +0200

    Moved rand() function inside the interface

--- src/JavaFXVersion/sorting/SortAlgorithm.java
+++ src/JavaFXVersion/sorting/SortAlgorithm.java
@@ -5,6 +5,7 @@ import javafx.scene.layout.GridPane;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Random;
 
 /**
  * The common interface of most sorting algorithms
@@ -39,4 +40,25 @@ public interface SortAlgorithm {
     default <T extends Comparable<T>> List<T> sort(List<T> unsorted) {
         return Arrays.asList(sort(unsorted.toArray((T[]) new Comparable[unsorted.size()])));
     }
+
+    /**
+     * Questa funzione randomizza un vettore di Tail utilizzato la classe Random
+     *
+     * @param array Il vettore da randomizzare
+     */
+    public static <T extends Comparable<T>> void rand(T[] array) {
+        // Creating object for Random class
+        Random rd = new Random();
+        // Starting from the last element and swapping one by one.
+        for (int i = array.length - 1; i > 0; i--) {
+            // Pick a random index from 0 to i
+            int j = rd.nextInt(i + 1);
+            // Swap array[i] with the element at random index
+            T temp = array[i];
+            array[i] = array[j];
+            array[j] = temp;
+        }
+    }
+
+
 }
