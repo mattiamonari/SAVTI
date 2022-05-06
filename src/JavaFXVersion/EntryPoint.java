9bda6abef6ab3eee934b8afdc73d5a70506f7b25
Mattia Monari <mattiamonari1972@gmail.com>
2022-05-06 16:12:52 +0200

    Added onCloseRequest method for the Stage

--- src/JavaFXVersion/EntryPoint.java
+++ src/JavaFXVersion/EntryPoint.java
@@ -1,9 +1,12 @@
 package JavaFXVersion;
 
 import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.event.EventHandler;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.stage.Stage;
+import javafx.stage.WindowEvent;
 
 import java.awt.*;
 //TODO!!!!!!! Sistemare un po' il codice in generale, commentare in maniera più chiara e migliorare la GUI
@@ -23,6 +26,13 @@ public class EntryPoint extends Application {
         MainWindow mw = new MainWindow(stage);
         Scene scene = new Scene(mw, screenSize.getWidth(), screenSize.getHeight());
         stage.setMaximized(true);
+        stage.setOnCloseRequest(new EventHandler<WindowEvent>() {
+            @Override
+            public void handle(WindowEvent t) {
+                Platform.exit();
+                System.exit(0);
+            }
+        });
         stage.setScene(scene);
         stage.show();
         stage.setResizable(false);
