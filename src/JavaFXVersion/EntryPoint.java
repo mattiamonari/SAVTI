288d503a5a18393738ffc5fdf3e1b8cb80b71342
Mattia Monari <mattiamonari1972@gmail.com>
2022-06-20 15:51:11 +0200

    Added an icon to the program (/res/icon.png)

--- src/JavaFXVersion/EntryPoint.java
+++ src/JavaFXVersion/EntryPoint.java
@@ -26,6 +26,7 @@ public class EntryPoint extends Application {
         MainWindow mw = new MainWindow(stage);
         Scene scene = new Scene(mw, screenSize.getWidth(), screenSize.getHeight());
         stage.setMaximized(true);
+        stage.getIcons().add(new Image("icon.png"));
         stage.setOnCloseRequest(new EventHandler<WindowEvent>() {
             @Override
             public void handle(WindowEvent t) {
