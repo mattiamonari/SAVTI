9c3bbd77b3948d36db9c8e728a36d8aadfed2c77
Mattia Monari <mattiamonari1972@gmail.com>
2022-06-27 15:53:21 +0200

    Switched from self-creating a process which runs ffmpeg to a ffmpeg wrapper

--- src/JavaFXVersion/FFMPEG.java
+++ src/JavaFXVersion/FFMPEG.java
@@ -1,62 +1,80 @@
 package JavaFXVersion;
 
+import javafx.scene.control.ProgressBar;
+import net.bramp.ffmpeg.FFmpeg;
+import net.bramp.ffmpeg.FFmpegExecutor;
+import net.bramp.ffmpeg.FFmpegUtils;
+import net.bramp.ffmpeg.FFprobe;
+import net.bramp.ffmpeg.builder.FFmpegBuilder;
+import net.bramp.ffmpeg.job.FFmpegJob;
+import net.bramp.ffmpeg.probe.FFmpegProbeResult;
+import net.bramp.ffmpeg.progress.Progress;
+import net.bramp.ffmpeg.progress.ProgressListener;
+
 import java.io.File;
 import java.io.IOException;
 import java.util.Scanner;
-
+import java.util.concurrent.TimeUnit;
 //./ffmpeg -framerate 1 -i final%d.png -vf -vcodec libx264 -acodec aac -strict -2 -preset slow -pix_fmt yuv420p -r 30
 // prova1.mp4
 //-vf -vf "drawtext=fontfile=/path/to/font.ttf:text='Stack Overflow':fontcolor=white:fontsize=24:box=1:boxcolor=black@0.5:boxborderw=5:x=(w-text_w)/2:y=(h-text_h)/2"
 
 
-//? MAYBE THIS CLASS CAN EVEN EXTEND PROCESS TODO
 public class FFMPEG {
 
     private Process process = null;
 
-    public FFMPEG(File pathToFFMPEG, String outName, File outputDirectory, int framerate, File music) {
-        File pathToExecutable = pathToFFMPEG;
-        ProcessBuilder builder;
-        if(music == null)
-        {
-            builder = new ProcessBuilder( pathToExecutable.getAbsolutePath(), "-framerate",
-                    String.valueOf(framerate),
-                    "-y", "-i",
-                    "final%d.png","-vcodec","libx264","-acodec", "aac", "-strict", "2", "-preset", "slow",
-                    "-pix_fmt", "yuv420p", outName);
-        }
-        else
-        {
-            builder = new ProcessBuilder( pathToExecutable.getAbsolutePath(), "-framerate",
-                    String.valueOf(framerate),
-                    "-y", "-i","final%d.png", "-i", music.getAbsolutePath(), "-c", "copy", "-map", "0:v:0", "-map",
-                    "1:a:0","-shortest", "-vcodec" ,"libx264", "-acodec", "aac", "-strict", "2", "-preset", "slow",
-                    "-pix_fmt",
-                    "yuv420p", outName);
-        }
-        builder.directory(outputDirectory);
-        builder.redirectErrorStream(true);
+    FFmpeg ffmpeg;
+    FFprobe ffprobe;
+    FFmpegBuilder builder;
+    FFmpegJob job;
+    double percentage;
+/*
+ "-i", music.getAbsolutePath(), "-c", "copy", "-map", "0:v:0", "-map",
+                    "1:a:0","-shortest"
+
+ */
+    public FFMPEG(UserSettings userSettings, ProgressBar progressBar) {
         try {
-            process = builder.start();
+            ffprobe = new FFprobe(userSettings.getFfprobePath().getAbsolutePath());
+            FFmpegProbeResult in = ffprobe.probe(userSettings.getOutputDirectory().getAbsolutePath() + "\\final%d.png");
+            ffmpeg = new FFmpeg(userSettings.getFfmpegPath().getAbsolutePath());
+            builder = new FFmpegBuilder()
+                    .setInput(in)
+                    .overrideOutputFiles(true)
+                    .addOutput(userSettings.getOutputDirectory().getAbsolutePath() + "\\" + userSettings.getOutName())
+                    .addExtraArgs("-shortest")
+                    .setAudioCodec("aac")
+                    .setVideoCodec("libx264")
+                    .setVideoFrameRate(userSettings.getFrameRate())
+                    .setPreset("slow")
+                    .setVideoPixelFormat("yuv420p")
+                    .done();
+
+            if(userSettings.getMusic() != null)
+            {
+                builder = builder.addInput(userSettings.getMusic().getAbsolutePath());
+
+            }
+
+            FFmpegExecutor executor = new FFmpegExecutor(ffmpeg, ffprobe);
+            job = executor.createJob(builder, new ProgressListener() {
+
+                // Using the FFmpegProbeResult determine the duration of the input
+                final double duration_ns = in.getFormat().duration * TimeUnit.SECONDS.toNanos(1);
+
+                @Override
+                public void progress(Progress progress) {
+                    percentage = progress.out_time_ns / duration_ns;
+                    progressBar.setProgress(percentage);
+                }
+
+            });
+            job.run();
+
         } catch (IOException e) {
             e.printStackTrace();
         }
-
-        Scanner s = new Scanner(process.getInputStream());
-        StringBuilder text = new StringBuilder();
-        while (s.hasNextLine()) {
-            text.append(s.nextLine());
-            text.append("\n");
-        }
-
-        s.close();
-        int result = 0;
-        try {
-            result = process.waitFor();
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-        System.out.println(text);
-        System.out.printf( "Process exited with result %d", result );
     }
+
 }
