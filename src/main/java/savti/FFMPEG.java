cb06fbae2efd202a797c3c6f4f519733adde4172
Mattia Monari <mattiamonari1972@gmail.com>
2022-11-15 22:32:27 +0100

    Deprecated

--- src/main/java/savti/FFMPEG.java
+++ src/main/java/savti/FFMPEG.java
@@ -16,41 +16,5 @@ import java.io.IOException;
 import java.util.concurrent.TimeUnit;
 
 public class FFMPEG {
-    FFmpeg ffmpeg;
-    FFprobe ffprobe;
-    FFmpegBuilder builder;
-    FFmpegJob job;
-    double percentage;
 
-    public FFMPEG(UserSettings userSettings, ProgressBar progressBar) {
-        try {
-            ffprobe = new FFprobe(userSettings.getFfprobePath().getAbsolutePath());
-            if (!ffprobe.isFFprobe()) {
-                Platform.exit();
-            }
-            ffmpeg = new FFmpeg(userSettings.getFfmpegPath().getAbsolutePath());
-            if (!ffmpeg.isFFmpeg()) {
-                Platform.exit();
-            }
-            FFmpegProbeResult in = ffprobe.probe(userSettings.getOutputDirectory().getAbsolutePath() + "\\final%d" + ".jpg");
-            builder = new FFmpegBuilder().addExtraArgs("-framerate", String.valueOf(userSettings.getFrameRate())).setInput(in).overrideOutputFiles(true).addOutput(userSettings.getOutputDirectory().getAbsolutePath() + "\\" + userSettings.getOutName()).setVideoFrameRate(userSettings.getFrameRate()).setAudioCodec("aac").setVideoCodec("libx264").setPreset("slow").setVideoPixelFormat("yuv420p").addExtraArgs("-shortest", "-crf", "18", "-vf", "\"crop=trunc(iw/2)*2:trunc(ih/2)*2,loop=" + userSettings.getFrameRate() * 2 + ":1:" + userSettings.getStartingImageIndex() + ",setpts=N/FRAME_RATE/TB\"").done();
-            if (userSettings.getMusic() != null) {
-                builder = builder.addInput(userSettings.getMusic().getAbsolutePath());
-            }
-            FFmpegExecutor executor = new FFmpegExecutor(ffmpeg, ffprobe);
-            job = executor.createJob(builder, new ProgressListener() {
-                // Using the FFmpegProbeResult determine the duration of the input
-                final double duration_ns = in.getFormat().duration * TimeUnit.SECONDS.toNanos(1);
-
-                @Override
-                public void progress(Progress progress) {
-                    percentage = progress.out_time_ns / duration_ns;
-                    progressBar.setProgress(percentage);
-                }
-            });
-            job.run();
-        } catch (IOException e) {
-            Platform.runLater(ErrorUtilities::FFError);
-        }
-    }
 }
